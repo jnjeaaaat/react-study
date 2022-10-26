942f225387f6f04a499a7b88effcf1b2ba093ce4
parktj <ekswn120@gmail.com>
2022-10-26 23:01:10 +0900

    7장 훅 useState(), useEffect() 실습

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock'
 import CommentList from './chapter_05/CommentList'
 import NotificationList from './chapter_06/NotificationList'
+import Accommodate from './chapter_07/Accommodate'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(
@@ -20,7 +21,7 @@ import NotificationList from './chapter_06/NotificationList'
 
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>,
   document.getElementById('root')
 );
