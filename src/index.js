63d9cd802ff70e19143d8fa5cb0036a59fe72995
parktj <ekswn120@gmail.com>
2022-10-27 22:58:16 +0900

    9장 리스트와 키 실습

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import NotificationList from './chapter_06/NotificationList'
 import Accommodate from './chapter_07/Accommodate'
 import ConfirmButton from './chapter_08/ConfirmButton'
 import LandingPage from './chapter_09/LandingPage'
+import AttendanceBook from './chapter_10/AttendanceBook'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(
@@ -23,7 +24,7 @@ import LandingPage from './chapter_09/LandingPage'
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <AttendanceBook />
   </React.StrictMode>,
   document.getElementById('root')
 );
