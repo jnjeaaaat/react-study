616b6ce5d0b67c465ab9c39eb3bd7bb602e66501
parktj <ekswn120@gmail.com>
2022-10-28 17:19:34 +0900

    12장 state 끌어올리기 실습

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import ConfirmButton from './chapter_08/ConfirmButton'
 import LandingPage from './chapter_09/LandingPage'
 import AttendanceBook from './chapter_10/AttendanceBook'
 import SignUp from './chapter_11/SignUp'
+import Calculator from './chapter_12/Calculator'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(
@@ -25,7 +26,7 @@ import SignUp from './chapter_11/SignUp'
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
