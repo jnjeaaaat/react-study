dc4f101b7e48b221d48cd73d30ba5874468bfee8
parktj <ekswn120@gmail.com>
2022-12-09 02:59:02 +0900

    13장 합성vs.상속 실습

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import LandingPage from './chapter_09/LandingPage'
 import AttendanceBook from './chapter_10/AttendanceBook'
 import SignUp from './chapter_11/SignUp'
 import Calculator from './chapter_12/Calculator'
+import ProfileCard from './chapter_13/ProfileCard'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(
@@ -26,7 +27,7 @@ import Calculator from './chapter_12/Calculator'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>,
   document.getElementById('root')
 );
