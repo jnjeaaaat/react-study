bb8d7143b0f6d190f6702a54cf405ecca37e7711
parktj <ekswn120@gmail.com>
2022-12-21 21:02:25 +0900

    14장 컨텍스트 실습

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import AttendanceBook from './chapter_10/AttendanceBook'
 import SignUp from './chapter_11/SignUp'
 import Calculator from './chapter_12/Calculator'
 import ProfileCard from './chapter_13/ProfileCard'
+import DarkOrLight from './chapter_14/DarkOrLight'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(
@@ -27,7 +28,7 @@ import ProfileCard from './chapter_13/ProfileCard'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>,
   document.getElementById('root')
 );
