ad6258034e1e3e6f512ebec845ddd2125484214a
parktj <ekswn120@gmail.com>
2023-01-01 19:37:06 +0900

    15장 스타일링 실습

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import SignUp from './chapter_11/SignUp'
 import Calculator from './chapter_12/Calculator'
 import ProfileCard from './chapter_13/ProfileCard'
 import DarkOrLight from './chapter_14/DarkOrLight'
+import Blocks from './chapter_15/Blocks'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // ReactDOM.render(
@@ -28,7 +29,7 @@ import DarkOrLight from './chapter_14/DarkOrLight'
 
 ReactDOM.render(
   <React.StrictMode>
-    <DarkOrLight />
+    <Blocks />
   </React.StrictMode>,
   document.getElementById('root')
 );
